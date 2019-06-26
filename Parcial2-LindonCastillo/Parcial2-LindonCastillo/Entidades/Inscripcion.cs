38cde26ff53750ce30f9c5b1ae6602f96691a15e
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2019-06-26 17:53:16 -0400

    Entidades Creadas

---
+++ Parcial2-LindonCastillo/Parcial2-LindonCastillo/Entidades/Inscripcion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_LindonCastillo.Entidades
+{
+    public class Inscripcion
+    {
+        [Key]
+        public int InscripcionId { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Monto { get; set; }
+        public virtual List<InscripcionDetalle> Detalle { get; set; }
+
+        public Inscripcion()
+        {
+            InscripcionId = 0;
+            Fecha = DateTime.Now;
+            Monto = 0;
+
+            Detalle = new List<InscripcionDetalle>();
+        }
+    }
+}
---
+++ Parcial2-LindonCastillo/Parcial2-LindonCastillo/Entidades/InscripcionDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_LindonCastillo.Entidades
+{
+    public class InscripcionDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int InscripcionId { get; set; }
+        public int EstudianteId { get; set; }
+
+        public InscripcionDetalle()
+        {
+            Id = 0;
+            InscripcionId = 0;
+            EstudianteId = 0;
+
+        }
+    }
+}
